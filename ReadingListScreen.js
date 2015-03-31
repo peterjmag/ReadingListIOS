4deca7809ccb9c118d4639d1185a8915ac369f63
Peter Magenheimer <peter.magenheimer@gmail.com>
2015-03-31 19:19:59 +0200

    Initial list view with static data and simple rows.

--- ReadingListScreen.js
+++ ReadingListScreen.js
@@ -3,14 +3,37 @@
 var React = require('react-native');
 var {
   StyleSheet,
+  ListView,
   View,
+  Text,
 } = React;
 
+var links = require('./staticData');
+
 var ReadingListScreen = React.createClass({
+  getInitialState: function() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
+
+    return {
+      links: ds.cloneWithRows(links)
+    };
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
+        <ListView
+          dataSource={this.state.links}
+          renderRow={this.renderRow}
+        />
+      </View>
+    );
+  },
 
+  renderRow: function(link) {
+    return (
+      <View>
+        <Text>{link.title}</Text>
       </View>
     );
   },
---
+++ staticData.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {"id":1427821959568,"url":"https://facebook.github.io/react-native/","title":"React Native | A framework for building native apps using React","host":"facebook.github.io","image":"http://facebook.github.io/react-native/img/opengraph.png?2","_id":"Wx2ZFUyeMzapTKPx"},
+    {"id":1427821950971,"url":"https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0","title":"Smart and Dumb Components — Medium","host":"medium.com","image":"https://d262ilb51hltx0.cloudfront.net/max/800/1*TatfeANQMBmjrk_nhKbZoA.jpeg","_id":"sq9rE9Ppvd9I2uNY"},
+    {"id":1427821963844,"url":"http://www.meetup.com/React-Berlin/events/221080348/","title":"React Berlin - Meetup #1 - React Berlin (Berlin) - Meetup","host":"www.meetup.com","image":"http://photos2.meetupstatic.com/photos/event/c/7/5/0/highres_434871024.jpeg","_id":"gkD4NXx0y9hVNVQI"},
+    {"id":1427821955439,"url":"https://twitter.com/peterjmag","title":"THIS IS STATIC DATA, PETER. CHANGE IT.","host":"twitter.com","_id":"UROgJOywB7rgb3BT"},
+    {"id":1427821917266,"url":"https://github.com/facebook/react","title":"facebook/react · GitHub","host":"github.com","image":"https://avatars2.githubusercontent.com/u/69631?v=3&s=400","_id":"V9lVBA1M9qSLnBnR"},
+    {"id":1427821936212,"url":"http://goinswriter.com/travel-young/","title":"3 Reasons to Travel While You're Young","host":"goinswriter.com","_id":"D8LU0YgfiEZ63aCr"},
+    {"id":1427821922067,"url":"https://devmynd.com/blog/2015-1-pairing-with-junior-developers","title":"DevMynd Blog: Pairing with Junior Developers","host":"devmynd.com","_id":"UEKykwLOgXFHU2Ls"},
+    {"id":1427821946150,"url":"https://facebook.github.io/react/","title":"A JavaScript library for building user interfaces | React","host":"facebook.github.io","image":"http://facebook.github.io/react/img/logo_og.png","_id":"jpCyoxPH7Oin3rJQ"},
+    {"id":1427821908000,"url":"http://www.fastcompany.com/3043082/most-creative-people/why-software-maker-fog-creek-is-helping-its-competitors-hire-women","title":"Why Software Maker Fog Creek Is Helping Its Competitors Hire Women | Fast Company | Business + Innovation","host":"www.fastcompany.com","image":"http://h.fastcompany.net/multisite_files/fastcompany/imagecache/620x350/poster/2015/03/3043082-poster-1280-fogcreek-women-hire.jpg","_id":"lJdRz1C23ewkkcof"},
+    {"id":1427821902662,"url":"http://www.researchgate.net/careers","title":"Careers - ResearchGate","host":"www.researchgate.net","_id":"DxBKWyjUMhRy9xTG"},
+    {"id":1427821899214,"url":"http://visionmedia.github.io/superagent/","title":"SuperAgent - Ajax with less suck","host":"visionmedia.github.io","_id":"xr1J4et1gHQCR57E"},
+    {"id":1427821850533,"url":"http://javascriptweekly.com/","title":"JavaScript Weekly: A Free, Weekly Email Newsletter","host":"javascriptweekly.com","_id":"ejtbJnXJi1cTZDSi"}
+];
